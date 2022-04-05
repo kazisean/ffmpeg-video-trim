61f57b571a0c86b9c6d7c95803b88fd089ef317f
Kazi Sean <81493533+kazisean@users.noreply.github.com>
2022-04-04 23:34:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ times_in = ("Enter the start time of where you want the video to be start from. 
 times_out = (" Enter the end time of where you want the video to be cut out. Example: 03:20:59")
 video_name = ("ENTER NAME OF THE VIDEO FILE YOU WANT TO TRIM with the file extention (eample : testvideo.mp4)")
 
-w = open("Enter the file where you want your ffmpeg code to be written it must be a .bat folder. Example: D:\Vod Review\Reviews\commands.bat", "a")  # append mode
+w = open("Enter the file where you want your ffmpeg code to be written it must be a .bat file. Make sure to edit the file with first and start it with @echo off. Example: D:\Vod Review\Reviews\commands.bat", "a")  # append mode
 w.write("\n")
 w.write("ffmpeg -i " + video_name + " -ss: " + times_in + " -to " + times_out + " -c:v copy -c:a copy " + file_name +".mp4")
 w.close()
